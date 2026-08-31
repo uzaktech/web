898364c0fe09922d25704663101336e987848675
enzoKazuki <enzo.kazuki9@gmail.com>
2026-08-31 16:34:38 -0300

    maximum scale meta viewport back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
 		<html lang="en">
 			<head>
 				<meta charSet="UTF-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1.0" />
 				<link rel="icon" sizes="144x144" href="favicon.png" type="image/png" />
 				<script
 					type="application/ld+json"
