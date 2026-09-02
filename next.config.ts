a49c58202f9e84360ebb96162c1ef8ef75543ad9
enzoKazuki <enzo.kazuki9@gmail.com>
2026-09-02 09:33:45 -0300

    uninstall of babel-plugin-react-compiler package

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   	devIndicators: false,
-	reactCompiler: false,
   	reactStrictMode: true,
 	compiler: {
 		styledComponents: {
