2fc67c2201879c68eef8a475b013034fa3cf270b
enzoKazuki <enzo.kazuki9@gmail.com>
2026-09-03 16:57:45 -0300

    fix deploy

--- src/providers/pageLoadFormatter.tsx
+++ src/providers/pageLoadFormatter.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 export const PageLoadFormatter = () => {
 	const pathname = usePathname();
-	const searchParams = useSearchParams();
 
 	useEffect(() => {
 		document.querySelector("[data-scroll-root]")?.scrollTo({top: 0, left: 0, behavior: "instant"})
-	}, [pathname, searchParams])
+	}, [pathname])
 
-	return null;
+	return <></>;
 }
